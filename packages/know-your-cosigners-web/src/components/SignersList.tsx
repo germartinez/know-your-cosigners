c20c8005e8b939123bc80a1de0c38b62f8d65a3b
Germán Martínez <germaga92@gmail.com>
2024-09-02 03:01:16 +0200

    Order signers by engagement

--- packages/know-your-cosigners-web/src/components/SignersList.tsx
+++ packages/know-your-cosigners-web/src/components/SignersList.tsx
@@ -1,5 +1,6 @@
 import { isSameAddress } from '@/logic/statistics'
 import { useStatistics } from '@/store'
+import { Signer } from '@/types'
 import * as d3 from 'd3'
 import { useState } from 'react'
 import { formatEther } from 'viem'
@@ -15,9 +16,17 @@ export default function SignerList() {
 
   const getColor = d3.scaleOrdinal(d3.schemeCategory10)
 
+
+  const orderedSigners = signersData?.sort((a: Signer, b: Signer) => a.totalSafeTxExecuted > b.totalSafeTxExecuted ? -1 : 1)
+    .map(signer => signer.address)
+
+  const data = signersData
+    ? orderedSigners
+    : safeOwners
+
   return (
     <ol>
-      {safeOwners && safeOwners.map((owner, i) => {
+      {data && data.map((owner, i) => {
         const signer = signersData?.filter(signer => isSameAddress(signer.address, owner))[0]
 
         return (
@@ -41,15 +50,17 @@ export default function SignerList() {
                   <div className="bar-box">
                     <div className="bar-value" style={{
                       backgroundColor: getColor(i.toString()),
-                      width: `${signer && signer.totalSafeTxExecuted && signer.totalTxExecuted
+                      width: `${signer && signer.totalTxExecuted > 0
                         ? signer.totalSafeTxExecuted * 130 / signer.totalTxExecuted
                         : 0
                       }px`
                     }}></div>
                   </div>
                   <div>
-                    {signer && signer.totalSafeTxExecuted && signer.totalTxExecuted
-                      ? `${signer.totalSafeTxExecuted} / ${signer.totalTxExecuted}`
+                    {signer
+                      ? signer.totalTxExecuted > 0
+                        ? `${signer.totalSafeTxExecuted} / ${signer.totalTxExecuted} (${(signer.totalSafeTxExecuted / signer.totalTxExecuted * 100).toFixed(2)}%)`
+                        : '0 (0%)'
                       : 'Loading...'
                     }
                   </div>
@@ -60,14 +71,16 @@ export default function SignerList() {
                   <div className="bar-box">
                     <div className="bar-value" style={{
                       backgroundColor: getColor(i.toString()),
-                      width: `${signer && signer.totalSafeGasUsed && signer.totalGasUsed
+                      width: `${signer && signer.totalGasUsed > 0
                         ? signer.totalSafeGasUsed * BigInt(130) / signer.totalGasUsed
                         : 0
                       }px`
                     }}></div>
                   </div>
-                  {signer && signer.totalSafeGasUsed && signer.totalGasUsed
-                    ? `${signer.totalSafeGasUsed} / ${signer.totalGasUsed}`
+                  {signer
+                    ? signer.totalGasUsed > 0
+                      ? `${signer.totalSafeGasUsed} (${Number(signer.totalSafeGasUsed / signer.totalGasUsed * BigInt(100)).toFixed(2)}%)`
+                      : '0 (0%)'
                     : 'Loading...'
                   }
                 </div>
@@ -77,15 +90,17 @@ export default function SignerList() {
                   <div className="bar-box">
                     <div className="bar-value" style={{
                       backgroundColor: getColor(i.toString()),
-                      width: `${signer && signer.totalSafeTxFees && signer.totalTxFees
+                      width: `${signer && signer.totalTxFees
                         ? signer.totalSafeTxFees * BigInt(130) / signer.totalTxFees
                         : 0
                       }`
                     }}></div>
                   </div>
                   <div>
-                    {signer && signer.totalSafeTxFees && signer.totalTxFees
-                      ? `${(+formatEther(signer.totalSafeTxFees)).toFixed(4)} / ${(+formatEther(signer.totalTxFees)).toFixed(4)} ETH`
+                    {signer
+                      ? signer.totalTxFees > 0
+                        ? `${(+formatEther(signer.totalSafeTxFees)).toFixed(4)} ETH (${Number((signer.totalSafeTxFees / signer.totalTxFees * BigInt(100))).toFixed(2)}%)`
+                        : '0 ETH (0%)'
                       : 'Loading...'
                     }
                   </div>
