2506a835f3a2684eb42e6fbf2b738684ce91a6c4
Germán Martínez <germaga92@gmail.com>
2024-09-07 06:56:20 +0100

    Get transactions from transaction Service

--- packages/know-your-cosigners-web/src/logic/safe.ts
+++ packages/know-your-cosigners-web/src/logic/safe.ts
@@ -1,4 +1,5 @@
 import safeAbi from '@/abi/safe.json'
+import { SafeTransaction } from '@/types'
 import { createPublicClient, getContract, http } from 'viem'
 import { mainnet } from 'viem/chains'
 
@@ -6,16 +7,13 @@ export async function getOwners(safeAddress: string, chainId: number): Promise<s
   if (chainId !== 1) {
     throw new Error('Network not supported.')
   }
-
   if (safeAddress === '') {
     return []
   }
-
   const publicClient = createPublicClient({
     chain: mainnet,
     transport: http(),
   })
-  
   try {
     const safe = getContract({
       address: safeAddress as `0x${string}`,
@@ -25,6 +23,41 @@ export async function getOwners(safeAddress: string, chainId: number): Promise<s
     const owners = await safe.read.getOwners() as string[]
     return owners
   } catch(e) {
+    return []
+  }
+}
+
+type SafeTransactionProps = {
+  safeAddress: string
+  chainName: string
+}
+
+export async function getSafeTransactions({
+  safeAddress,
+  chainName,
+}: SafeTransactionProps): Promise<SafeTransaction[]> {
+  try {
+    let transactions: SafeTransaction[] = []
+    let next = `https://safe-transaction-${chainName}.safe.global/api/v1/safes/${safeAddress}/multisig-transactions`
+    while (next) {
+      const request = await fetch(
+        next,
+        {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+      const response = await request.json()
+      transactions = transactions.concat(response.results)
+      next = response.next
+      console.log(transactions.length, next)
+    }
+    return transactions
+  }
+  catch(error: any) {
     return []
   }
 }
